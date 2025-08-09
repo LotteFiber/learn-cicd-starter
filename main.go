8c055de04ac560e8821d89dd1ff53addd0764a90
LotteFiber <ratchanonnine9@gmail.com>
2025-08-09 21:57:02 +0700

    fix all severity code #2

--- main.go
+++ main.go
@@ -5,9 +5,9 @@ import (
 	"embed"
 	"io"
 	"log"
-	"time"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
@@ -90,9 +90,9 @@ func main() {
 
 	router.Mount("/v1", v1Router)
 	srv := &http.Server{
-		Addr:    ":" + port,
-		Handler: router,
-		ReadHeaderTimeout: 5 * time.Second
+		Addr:              ":" + port,
+		Handler:           router,
+		ReadHeaderTimeout: 5 * time.Second,
 	}
 
 	log.Printf("Serving on port: %s\n", port)
